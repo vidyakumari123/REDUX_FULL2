6e71d5057192c35563053ff06b0882f19387117f
vidyakumari123 <127233800+vidyakumari123@users.noreply.github.com>
2026-02-26 18:12:59 +0530

    Chap 4 - Redux Toolkit QUERY for REST APIs

--- src/App.js
+++ src/App.js
@@ -2,27 +2,26 @@ import { useSelector } from 'react-redux';
 import './App.css';
 import Account from './components/Account';
 import Bonus from './components/Bonus';
+import Reward from './components/Reward';
+import Admin from './components/Admin';
+
 function App() {
-  const amount = useSelector((state) => state.account.amount);
-  const points = useSelector((state) => state.bonus.points);
-  const account = useSelector((state) => state.account);
+
+  const amount= useSelector(state=>state.account.amount);
+  const points= useSelector(state=>state.bonus.points)
+
   return (
     <div className="App">
       <h4>App</h4>
-      {account.pending ? (
-        <p>loading....</p>
-      ) : account.error ? (
-        <p>{account.error}</p>
-      ) : (
-        <h3>Current Amount : {amount} </h3>
-      )}
+      <h3>Current Amount : {amount} </h3>
       <h3>Total Bonus : {points}</h3>
-
       <Account></Account>
       <Bonus></Bonus>
+      <Reward></Reward>
+      <Admin></Admin>
+
     </div>
   );
 }
-
 
 export default App;
